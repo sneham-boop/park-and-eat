6342ca5fa409c16e0cf14f5dd8619ceb9013bf83
Sneha Mahajan <sneh.km@gmail.com>
2022-12-29 15:23:41 -0500

    Use auth provider.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import './App.css';
-// require("dotenv").config();
 import Map from "./components/Map";
-
+import { useContext } from "react";
+import "./App.css";
+import { authContext } from "./providers/AuthProvider";
 
 function App() {
+  const { auth } = useContext(authContext);
   return (
     <div className="App">
       <div>Welcome to PARKEATS.</div>
-      <Map/>
+      {auth ? (
+        <p>You have been authenticated.</p>
+      ) : (
+        <p>Please log in to proceed.</p>
+      )}
+      <Map />
     </div>
   );
 }
--- src/providers/AuthProvider.js
+++ src/providers/AuthProvider.js
@@ -1,10 +1,17 @@
-import React, { useState, createContext } from "react";
+import React, { useState, createContext, useEffect } from "react";
 
 export const authContext = createContext();
 
 export default function AuthProvider(props) {
-  const [auth, setAuth] = useState(true);
+  const [auth, setAuth] = useState(false);
+
+  // Default state change to authenticated prior to backend setup.
+  useEffect(() => {
+    setAuth(false);
+  }, []);
+
   const data = { auth };
+
   return (
     <authContext.Provider value={data}>{props.children}</authContext.Provider>
   );
